8c11cb2beba8502b2a8b76be4efa89e0d29e75b9
Szesny32 <bartoszszczesny.studia@gmail.com>
2022-05-04 17:35:18 +0200

    PlayerStatus refactor

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -12,19 +12,39 @@ public class PlayerManager : MonoBehaviour
     private Vector3 cameraRotation = Vector3.zero;
     [SerializeField] private float cameraMinRange = -30.0f;
     [SerializeField] private float cameraMaxRange = 60.0f;
+
+    [SerializeField] private float walkSpeed = 4.0f;
+    [SerializeField] private float runSpeed = 6.0f;
+    [SerializeField] private float jumpHeight = 0.6f;
+    [SerializeField] private float gravityAcceleration = -9.81f;
+    
+    private CharacterController controller;
+    private PlayerStatus playerStatus;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 direction = Vector3.zero;
     void Start()
     {
         setModel(modelPrefab);
         playerCamera = transform.Find("Camera").gameObject;
         Cursor.lockState = CursorLockMode.Locked;
         refreshCameraPosition();
+        
+        controller = GetComponent<CharacterController>(); //shift alt down
+        playerStatus = GetComponent<PlayerStatus>();
+        setController(0.55f, 0.2f);
     }
 
     void Update()
     {
-
         if(debug)
-        {
+            shootingTest();
+        gravity();
+        movement();
+        mouse();
+    }
+
+void shootingTest()
+{
             Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
             Debug.DrawRay(ray.origin, ray.direction*20.0f, Color.red);
             RaycastHit hit;
@@ -38,8 +58,9 @@ public class PlayerManager : MonoBehaviour
                         HP.takeDamage(25.0f);
                     }  
             }
-        }
-    }
+
+}
+
     private void setModel(GameObject prefab)
     {
         if(prefab != null)
@@ -60,15 +81,97 @@ public class PlayerManager : MonoBehaviour
             cameraRotation = Vector3.zero;
         }
     }
-    public void rotateCamera(Vector3 rotation)
+
+
+    void gravity()
     {
+        //velocity = acceleration * time
+        float lossOfVelocity = gravityAcceleration * Time.deltaTime;
+        if(controller.isGrounded)
+                velocity.y = lossOfVelocity;
+        else
+            velocity.y += lossOfVelocity;
+    }
+    void mouse()
+    {
+        //distance = velocity * time
+        float mouseX = mouseSensitivity* Input.GetAxis("Mouse X") * Time.deltaTime;
+        float mouseY = mouseSensitivity * Input.GetAxis("Mouse Y") * Time.deltaTime;
+        Vector3 rotation = new Vector3(-mouseY, 0, 0);
         cameraRotation += rotation;
         cameraRotation.x =  Mathf.Clamp(cameraRotation.x, cameraMinRange, cameraMaxRange);
         playerCamera.transform.localRotation = Quaternion.Euler(cameraRotation);
+        transform.Rotate(Vector3.up * mouseX);
     }
-    public float MouseSenstivity()
+
+    void movement()
     {
-        return mouseSensitivity;
+        direction = Vector3.zero;
+        direction += transform.right * Input.GetAxis("Horizontal") ;
+        direction += transform.forward * Input.GetAxis("Vertical");
+        float speed = 0.0f;
+        
+        if(controller.isGrounded)
+        {//WASD
+
+            if(direction.x != 0.0f || direction.z != 0.0f)
+                {
+                    speed = Input.GetKey(KeyCode.LeftShift)? runSpeed : walkSpeed;
+                    playerStatus.state = Input.GetKey(KeyCode.LeftShift)? PlayerStatus.State.Run : PlayerStatus.State.Walk;
+                }
+            else
+                playerStatus.state = PlayerStatus.State.Idlee;
+            
+            //JUMP
+            if(Input.GetKey(KeyCode.Space) && controller.isGrounded)
+                {
+                    velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityAcceleration);
+                    playerStatus.state = PlayerStatus.State.Jump;
+                }
+        
+            velocity.x = speed*direction.x;
+            velocity.z = speed*direction.z;
+        }
+        else
+        {
+            if(direction.x != 0.0f || direction.z != 0.0f)
+                speed = walkSpeed;
+            if(velocity.y > 0)
+                playerStatus.state = PlayerStatus.State.Rises; 
+            else
+                playerStatus.state = PlayerStatus.State.Falls; 
+        }
+        //MOVE BY DISTANCE (distance = velocity * time)
+        Vector3 distance = velocity * Time.deltaTime;
+        controller.Move(distance);
     }
+    
+
+    void setController(float radius,float stepOffset)
+    {
+        Vector3 headPoint = transform.Find("Model/Hips/Spine/Spine1/Spine2/Neck/Head/HeadTop_End").position;
+        controller.height = headPoint.y - transform.position.y;
+        controller.radius = radius;
+        controller.skinWidth = 0.1f * controller.radius;
+        controller.center = new Vector3(0.0f, ((controller.height/2.0f) + controller.skinWidth) ,0.0f);
+        controller.minMoveDistance = 0.0f;
+        controller.stepOffset = stepOffset;
+    }
+
+    
+
+    public Vector3 getVelocity()
+    {
+        return velocity;
+    }
+    public Vector3 getDirection()
+    {
+        return direction;
+    }
+    public bool getIsGrounded()
+    {
+        return controller.isGrounded;
+    }
+
 }
 
--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -6,41 +6,24 @@ public class PlayerStatus : MonoBehaviour
 {
     public enum State {Idlee, Walk, Run, Jump, Falls, Rises, Aiming};
     
-    [HideInInspector] public State state; 
-    
+    public State state; 
     [SerializeField] private bool debug = false;
-   
-    private MovementController playerController;
+
 
     void Start()
     {
-        playerController = GetComponent<MovementController>();
         state = State.Idlee;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerController.getIsGrounded())
-        {
-            if(Input.GetKey(KeyCode.Space))
-                state = State.Jump;
-            else if(playerController.getDirection().x != 0.0f || playerController.getDirection().z != 0.0f)    
-                state = Input.GetKey(KeyCode.LeftShift)? State.Run : State.Walk; 
-            else
-               state = State.Idlee;
-        }
-        else
-        {
-            if(playerController.getVelocity().y > 0)
-                state = State.Rises; 
-            else
-                state = State.Falls; 
-        }
+
         if(debug)
             Debug.Log(state);
     }
+
+    
 }
 
 
