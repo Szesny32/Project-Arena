e44703a630eceaea805da1e6a6aa56ba8d0cd8ef
KacperJach <kacper.jach@vp.pl>
2022-05-18 22:21:53 +0200

    Added jump and running sounds, made a silencing effect

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -9,6 +9,8 @@ public class AudioController : NetworkBehaviour
     private PlayerManager playerManager;
     private AudioSource audioSource;
     private AudioClip footstep;
+    private AudioClip jump;
+    private AudioClip run;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,10 @@ public class AudioController : NetworkBehaviour
         playerStatus = GetComponent<PlayerStatus>();
         audioSource = GetComponent<AudioSource>();
         footstep = Resources.Load<AudioClip>("Classic Footstep SFX/Floor/Floor_step10");
+        run = Resources.Load<AudioClip>("Classic Footstep SFX/Ground/Ground_running_loop0");
+        jump = Resources.Load<AudioClip>("Classic Footstep SFX/Forest ground/Forest_ground_jump0");
     }
+
 
     // Update is called once per frame
     void Update()
@@ -48,7 +53,21 @@ public class AudioController : NetworkBehaviour
 
             case PlayerStatus.State.Run:
                 {
-                    
+                    if (!audioSource.isPlaying)
+                        {
+                            audioSource.clip = run;
+                            audioSource.PlayOneShot(run);
+                        }
+                }
+                break;
+
+            case PlayerStatus.State.Jump:
+                {
+                    if (!audioSource.isPlaying)
+                        {
+                            audioSource.clip = jump;
+                            audioSource.PlayOneShot(jump);
+                        }      
                 }
                 break;
 
@@ -74,6 +93,7 @@ public class AudioController : NetworkBehaviour
                           
                 }
                 break;
+                
 
             default:
                 // Debug.Log("Custom ERROR "+playerObject.name+" state undefined");
